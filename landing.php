200ae3f352bae15c7c1cc2b75844fd4c918e120c
JakeAdkisson <43857945+JakeAdkisson@users.noreply.github.com>
2018-12-04 22:53:04 -0500

    Add files via upload

--- landing.php
+++ landing.php
@@ -8,9 +8,8 @@
     <meta name="description" content="The round table of all things gaming. Take a seat.">
     <meta name="keywords" content="gaming site, gaming forum, esports forum">
     <meta name="author" content="JMichEvans, Jake Adkisson">
-    <!--<link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/main-mobile.css">
-    <script src="../js/landing.js"></script>-->
     
     <!-- Header -->
     <header>
@@ -28,10 +27,9 @@
 
       </div>
     </header>
-
+  </head>
     <!-- Brief Jumbotron w/header -->
     <!-- Top Forums -->
-
 <?php
   require 'dbh.inc.php';
   $image = isset($_POST['image']) ? $_POST['image']:'';
@@ -41,13 +39,15 @@
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
       // output data of each row
-      echo "<table>";
+      echo "<table class=\"feed\">";
       $count = 0;
       while($row = $result->fetch_assoc()) {
         echo "<tr>";
-          echo "<td><img src=\"".$row["Comment_Picture"]."\"></td>";
-          echo "<td>".$row["Comment_Text"]."</td>";
-          echo "<td> Likes: ".$row["Comment_Likes"];
+          echo "<td><img class=\"uploaded-image\" src=\"".$row["Comment_Picture"]."\"></td>";
+          echo "<td class=\"comments\">".$row["Comment_Text"]."</td>";
+          echo "<td class=\"comments\"> Likes: ".$row["Comment_Likes"]."</td>";
+          echo "<td class=\"comments\"><button type=\"button\" onclick=\"like_function()\">Like!</button></td>";
+
         echo "</tr>";
         $count = $count+1;
       }
@@ -59,7 +59,7 @@
    if(isset($image)){
       move_uploaded_file($_FILES["image"]["tmp_name"],"../img/" . $_FILES["image"]["name"]);
       $file="../img/".$_FILES["image"]["name"];
-      echo "FILE = ".$file;
+      //echo "FILE = ".$file;
       $sql = "INSERT INTO ".$table." (Comment_Text, Comment_Picture) VALUES ('$comment', '$file')";
       if ($conn->query($sql) === TRUE) {
         //echo "New record created successfully";
