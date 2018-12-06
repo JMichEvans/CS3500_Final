c02953d4fae9fc36781bf69e5093cf8abe7cb4be
JakeAdkisson <43857945+JakeAdkisson@users.noreply.github.com>
2018-12-05 23:50:56 -0500

    Add files via upload

--- php/landing.php
+++ php/landing.php
@@ -33,18 +33,55 @@
     <!-- Top Forums -->
 
 <?php
-  require 'message.php'
+  require "dbh.inc.php";
+  $image = isset($_POST['image']) ? $_POST['image']:'';
+  $comment = isset($_POST['text-upload']) ? $_POST['text-upload']:'';
+  $sql = "SELECT * FROM comment ORDER BY Comment_Likes ASC";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      echo "<table class=\"feed\">";
+      $count = 0;
+      while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+          echo "<td><img class=\"uploaded-image\" src=\"".$row["Comment_Picture"]."\"></td>";
+          echo "<td class=\"comments\">".$row["Comment_Text"]."</td>";
+          echo "<td  class=\"comments\"> Likes: <b id=\"".$count."\">".$row["Comment_Likes"]."</b></td>";
+          echo "<td class=\"comments\"><button type=\"button\" onclick=\"like_function('".$count."')\">Like!</button></td>";
+        echo "</tr>";
+        $count = $count+1;
+      }
+      
+      echo "</table>";
+  } else {
+      echo "0 results";
+  }
+   if(isset($image)){
+      move_uploaded_file($_FILES["image"]["tmp_name"],"../img/" . $_FILES["image"]["name"]);
+      $file="../img/".$_FILES["image"]["name"];
+      //echo "FILE = ".$file;
+      $sql = "INSERT INTO COMMENT (Comment_Text, Comment_Picture) VALUES ('$comment', '$file')";
+      if ($conn->query($sql) === TRUE) {
+        //echo "New record created successfully";
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+   }
+   elseif(isset($comment)){
+      $sql = "INSERT INTO COMMENT (Comment_Text) VALUES ('$comment')";
+      if ($conn->query($sql) === TRUE) {
+        //echo "New record created successfully";
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+   }
+  //require "message.php";
 ?>
-<div class="comment-test">
-          <p>Upload file or text:</p><br><br>
-          <form name="form-b" action="" method="POST" enctype="multipart/form-data">
-            <input id="comment" type="text"name="comment" required placeholder="Maximum 200 characters" capture>
-            <input type="submit" value="Submit">
-          </form> 
-    </div>
     <div class="upload-container">
           <p>Upload file or text:</p><br><br>
-          <form name="form" action="message.php" method="POST" enctype="multipart/form-data">
+          <form name="form" action="" method="POST" enctype="multipart/form-data">
             <input type="file" name="image" accept="image/*" capture>
             <input id="text-upload" type="text"name="text-upload" required placeholder="Maximum 200 characters" capture>
             <input type="submit" value="Submit">
