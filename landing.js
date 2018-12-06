de1e800db1e2b8f4a32b4682c81fadd1d6e94fea
JakeAdkisson <43857945+JakeAdkisson@users.noreply.github.com>
2018-12-06 05:22:56 -0500

    Delete landing.js

--- landing.js
+++
@@ -1,9 +0,0 @@
-function like_function(id){
-	var button = document.getElementById(id);
-	//console.log(button);
-	var num = button.innerHTML;
-	num++;
-	button.innerHTML = num;
-	//button.disabled = true;
-	button.disabled = true;
-}
